5112e6ac68868804f81727168b5b96ef221db9de
Heldenglanz <Heldenglanz@gmail.com>
2022-06-03 20:43:07 +0300

    ok

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const photoSet = createPhotoMarcup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", photoSet)
+
+console.log(createPhotoMarcup(galleryItems));
+
+function createPhotoMarcup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return`
+            <img 
+        class="gallery__image"
+        src = "${preview}"
+        alt = "${description}"
+            />`;
+    }).join('');
+    
+
+
+};
+
+
+
+preview.addEventListener('click', modalOpen);
+function modalOpen(e) {
+    
+}
+
+
