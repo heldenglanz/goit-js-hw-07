2d16d0527bc1f194277eed8fe6c38db08d782e5f
Heldenglanz <Heldenglanz@gmail.com>
2022-06-04 15:16:22 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,26 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
-const ulEl = document.createElement("ul");
-galleryContainer.appendChild(ulEl);
-ulEl.classList.add('gallery')
+
+
 
 const photoSet = createPhotoMarcup(galleryItems);
-ulEl.insertAdjacentHTML("beforeend", photoSet);
+
+
+
+galleryContainer.insertAdjacentHTML("beforeend", photoSet);
 
 console.log(createPhotoMarcup(galleryItems));
 
 function createPhotoMarcup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
-        return`
-        <li class="gallery__item">
-<a class="gallery__link">
+        return`<a class="gallery__link"
+        href="${original}">
             <img 
         class="gallery__image"
         src = "${preview}"
         alt = "${description}"
-            /></a>
-            </li>`;
+            /></a>`;
     }).join('');
     
 
