0a163874e1dcd530c8542c5f6135fa28efc748db
Heldenglanz <Heldenglanz@gmail.com>
2022-06-04 19:49:45 +0300

    emm...

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,37 +2,62 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
-
-
-
+let instance = '';
 const photoSet = createPhotoMarcup(galleryItems);
 
-
-
 galleryContainer.insertAdjacentHTML("beforeend", photoSet);
+
+galleryContainer.addEventListener('click', clickOnTheImg);
 
 console.log(createPhotoMarcup(galleryItems));
 
 function createPhotoMarcup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
-        return`<a class="gallery__link"
+        return`<div class="gallery__item">
+        <a class="gallery__link"
         href="${original}">
             <img 
         class="gallery__image"
         src = "${preview}"
+        data-source="${original}"
         alt = "${description}"
-            /></a>`;
+            /></a></div>`;
     }).join('');
     
-
-
 };
 
 
+function clickOnTheImg(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
 
-// preview.addEventListener('click', modalOpen);
-// function modalOpen(e) {
+const large = e.target.dataset.source;
+    openedModal(large);
+}
     
-// }
+function openedModal(large) {
+     document.body.addEventListener('click', closeModal);
+    document.body.classList.add('show-modal');
+   
+   
+       
 
+    instance = basicLightbox.create(
+        `<img 
+        src="${large}"
+    />
+`);
+    instance.show();
+}
+
+function closeModal(e) {
+    if (e.target === e.currentTarget) {
+    
+        document.body.classList.remove('show-modal');
+        document.body.removeEventListener('click', openedModal);
+    }
+
+}
 
